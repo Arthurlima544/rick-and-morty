8a3dd428007ab2f73454f63ba2047e4b6eecbdc1
Arthurlima544 <arthurlindaolima544@gmail.com>
2022-03-13 13:36:07 -0300

    add imersivity to the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:rick_and_morty/controller/pages/home_page/home_page_controller.dart';
 import 'package:rick_and_morty/view/home_page/home_page.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
   runApp(const MyApp());
 }
 
@@ -17,6 +20,7 @@ class MyApp extends StatelessWidget {
         create: (context) => HomePageController(),
         child: const HomePage(),
       ),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
