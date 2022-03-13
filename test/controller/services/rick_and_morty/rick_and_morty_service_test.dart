fa9191cd22b94f1ceee983b3e86155116bd5efe5
Arthurlima544 <arthurlindaolima544@gmail.com>
2022-03-13 15:14:55 -0300

    add tests for getCharacteres method

--- test/controller/services/rick_and_morty/rick_and_morty_service_test.dart
+++ test/controller/services/rick_and_morty/rick_and_morty_service_test.dart
@@ -1,9 +1,24 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:rick_and_morty/controller/services/rick_and_morty/client/dio_client.dart';
 import 'package:rick_and_morty/controller/services/rick_and_morty/rick_and_morty_service.dart';
+import 'package:rick_and_morty/model/character.dart';
 import 'package:rick_and_morty/model/episode.dart';
+import 'package:rick_and_morty/model/location.dart';
+import 'package:rick_and_morty/model/origin.dart';
 
 void main() {
+  const firstCaracter = Character(
+      id: 1,
+      name: "Rick Sanchez",
+      status: "Alive",
+      location: Location(
+          name: "Citadel of Ricks",
+          url: "https://rickandmortyapi.com/api/location/3"),
+      gender: "Male",
+      origin: Origin(
+          name: "Earth (C-137)",
+          url: "https://rickandmortyapi.com/api/location/1"),
+      urlImage: "https://rickandmortyapi.com/api/character/avatar/1.jpeg");
   const firstEpisode = Episode(
       id: 1,
       name: "Pilot",
@@ -31,6 +46,7 @@ void main() {
         "https://rickandmortyapi.com/api/character/435"
       ],
       created: "2017-11-10T12:56:33.798Z");
+
   test('Should make a request via Dio client and return all episodes in page 1',
       () async {
     // Arrage
@@ -40,5 +56,17 @@ void main() {
     // Assert
     expect(res, isA<List<Episode>>());
     expect(res[0], equals(firstEpisode));
+  });
+
+  test(
+      'Should make a request via Dio client and in list cotains first character ',
+      () async {
+    // Arrage
+    RickAndMortyService service = RickAndMortyService(ClientDio());
+    // Act
+    final res = await service.getCharactersInEpisode(firstEpisode);
+    // Assert
+    expect(res, isA<List<Character>>());
+    expect(res[0], equals(firstCaracter));
   });
 }
