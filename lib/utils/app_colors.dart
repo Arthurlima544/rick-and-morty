2f2a69d4f9affcce6af02398780fc4b156016956
Arthurlima544 <arthurlindaolima544@gmail.com>
2022-03-13 15:15:50 -0300

    add forLayer appColor

--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -4,4 +4,5 @@ class AppColors {
   static const Color primary = Color(0xFF353333);
   static const Color secondary = Color(0xFF575454);
   static const Color font = Color(0xFFFFFDFD);
+  static const Color forLayer = Color(0xFFAAAAAA);
 }
