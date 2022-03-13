1721fe457c908777d69d48c65769bd0a535ec5e8
Arthurlima544 <arthurlindaolima544@gmail.com>
2022-03-13 13:33:48 -0300

    add principal colors of app

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF353333);
+  static const Color secondary = Color(0xFF575454);
+  static const Color font = Color(0xFFFFFDFD);
+}
