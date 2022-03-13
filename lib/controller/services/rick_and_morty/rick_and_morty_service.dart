562e2232966477e479b3d1e6002da6b4a4130423
Arthurlima544 <arthurlindaolima544@gmail.com>
2022-03-13 15:14:10 -0300

    add getCharacteres method

--- lib/controller/services/rick_and_morty/rick_and_morty_service.dart
+++ lib/controller/services/rick_and_morty/rick_and_morty_service.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:rick_and_morty/controller/services/rick_and_morty/interface/http_client_interface.dart';
+import 'package:rick_and_morty/model/character.dart';
 import 'package:rick_and_morty/model/episode.dart';
 
 const baseUrl = "https://rickandmortyapi.com/api/";
@@ -9,7 +11,8 @@ class RickAndMortyService {
   RickAndMortyService(this.client);
 
   Future<List<Episode>> getEpisodes(int page) async {
-    // <List<Episode>>
+    // Todo: criar tratamento de erros
+    // Todo retornar infos dos Get episodes
     var response = await client.get("$baseUrl/episode?page=$page");
     var body = (response.data as Map<String, dynamic>);
     var json = body["results"] as List;
@@ -17,5 +20,16 @@ class RickAndMortyService {
       return Episode.fromJson(e);
     }).toList();
     return listEpisodes;
+  }
+
+  Future<List<Character>> getCharactersInEpisode(Episode episode) async {
+    List<Character> listCharacters = [];
+    for (dynamic characterUrl in episode.characteres) {
+      var response = await client.get(characterUrl);
+      var body = (response.data as Map<String, dynamic>);
+      listCharacters.add(Character.fromJson(body));
+    }
+
+    return listCharacters;
   }
 }
